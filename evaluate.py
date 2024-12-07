1170c7b9bd81cf53633d04645419df676f399d06
yy-gx <yyang9923@gmail.com>
2024-12-07 14:34:54 -0500

    add evaluate script

--- evaluate.py
+++ evaluate.py
@@ -177,13 +177,13 @@ def main() -> None:
         else:
             agent.load_pretrained_model(args.model_folder_path, f"last_ddpm_task_idx_{task_id}.pth")
         # Eval pre-trained agent in Libero simu env
-        sr = eval(cfg, task_embs, task_id, agent, seed=args.seed, is_osm=args.is_osm, mapping=mapping, task_suite = args.task_suite)
+        sr = eval(cfg, task_embs, task_id, agent, seed=args.seed, is_osm=args.is_osm, mapping=None, task_suite=args.task_suite)
         print(f">> Success Rate for {task_name}: {sr}")
         tasks_succ_ls.append(sr)
-        np.save(f"{args.model_folder_path}/succ_list_seed_{args.seed}.npy", np.array(tasks_succ_ls))
+        np.save(f"{args.model_folder_path}/succ_list_bm_{args.task_suite}_seed_{args.seed}.npy", np.array(tasks_succ_ls))
 
     print(f">> SR List: {tasks_succ_ls}")
-    np.save(args.model_folder_path, np.array(tasks_succ_ls))
+    np.save(f"{args.model_folder_path}/succ_list_bm_{args.task_suite}_seed_{args.seed}.npy", np.array(tasks_succ_ls))
 
 
 if __name__ == "__main__":
