1c91d2d40c7a0c3feade451bb190e089ee58a51a
yy-gx <yyang9923@gmail.com>
2024-12-07 19:07:27 -0500

    add evaluate script

--- script/bm3/long_horizon_task_eval_mail.sh
+++ script/bm3/long_horizon_task_eval_mail.sh
@@ -9,3 +9,4 @@ for task_order_index in {7..16}; do
 done
 
 
+export CUDA_VISIBLE_DEVICES=7 && export MUJOCO_EGL_DEVICE_ID=7 &&  python eval_skill_chain_new.py --task_order_index 7 --seed 10000 --device_id 0 --benchmark "libero_90"  --model_folder_path "/mnt/arc/yygx/pkgs_baselines/MaIL/checkpoints/separate_no_hand_500_epoch_ckpts/" --model_path_folder "/mnt/arc/yygx/pkgs_baselines/LIBERO/libero/experiments/libero_90/skill_policies_without_wrist_camera_view/Sequential/BCRNNPolicy_seed10000/all"
